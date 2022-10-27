46674421dae113d692edf60bef4c4a30d43c7b24
NicholasMendez11 <nicholasmeeendez@gmail.com>
2022-10-27 16:01:43 -0400

    Update

--- components/About.jsx
+++ components/About.jsx
@@ -1,41 +1,43 @@
 import React from "react";
-import Image from 'next/image';
-import Logo from '../public/logo.jpg'
+import Image from "next/image";
+import Logo from "../public/logo.jpg";
 const About = () => {
   return (
-    <div id='about' className="w-full md:h-screen p-2 flex items-center py-16">
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-10 ">
         <div className="col-span-2">
-          <p className=" uppercase text-xl tracking-widest text-[#004AAD]">About</p>
+          <p className=" uppercase text-xl tracking-widest text-[#004AAD]">
+            About
+          </p>
           <h2 className="py-4">WHO I AM</h2>
           <p className="py-2 text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat
-            blanditiis vero cupiditate, ducimus nam aliquam, dolorum
-            reprehenderit magnam odit saepe repellendus culpa adipisci odio
-            consequatur ut corrupti distinctio impedit?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat
-            blanditiis vero cupiditate, ducimus nam aliquam, dolorum
-            reprehenderit magnam odit saepe repellendus culpa adipisci odio
-            consequatur ut corrupti distinctio impedit?
-
+            Sotfware developer. Looking forward to expanding my skills and
+            knowledge within the software development industry, contributing
+            with ideas, solutions, and results in a meaningful way. A faithful
+            believer in teamwork, how this allows us to achieve more, and that
+            success is always better when is shared.
           </p>
 
-            <br/>
+          <br />
           <p className="py-2 text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat
-            blanditiis vero cupiditate, ducimus nam aliquam, dolorum
-            reprehenderit magnam odit saepe repellendus culpa adipisci odio
-            consequatur ut corrupti distinctio impedit?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat
-            blanditiis vero cupiditate, ducimus nam aliquam, dolorum
-            reprehenderit magnam odit saepe repellendus culpa adipisci odio
-            consequatur ut corrupti distinctio impedit?
+            2 years of experience. Creating systems to address business needs.
+            Focussed on building front-end web applications, cross-mobile
+            applications, Android native apps with jetpack Compose. I have
+            experience developing integrations for Zoho&apos;s CRM and BPM.
           </p>
-          <br/>
+          <br />
           <p className="py-2 text-gray-600 underline cursor-pointer">
             Check out some of my latest projects
           </p>
         </div>
         <div className=" w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-            <Image className="rounded-xl bg-gradient-to-r from-[#3a7bd5] to-[#E6DADA]" src={Logo} alt="/" width="600" height="500"/>
+          <Image
+            className="rounded-xl bg-gradient-to-r from-[#3a7bd5] to-[#E6DADA]"
+            src={Logo}
+            alt="/"
+            width="600"
+            height="500"
+          />
         </div>
       </div>
     </div>
