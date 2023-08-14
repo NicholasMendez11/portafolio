78b8614f303d0397b274aad714dad789186c0240
Nicholas Mendez <nicholas.mendez@idt.net>
2023-08-14 14:13:32 -0400

    Update index.js

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -17,7 +17,7 @@ const Testimonials = () => {
           exit='hidden'
           className='h2 mb-8 xl:mb-0'
         >
-          What clients <span className='text-accent'>say.</span>
+          What coworkers have <span className='text-accent'>said.</span>
         </motion.h2>
         {/* slider */}
         <motion.div
